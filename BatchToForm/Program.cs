0b8b976ef3a79c2fe9d92e02b0d0620fa7d4495a
Clijsters <dominique.clijsters@gmail.com>
2014-03-02 11:27:38 +0100

    Created solution
    Testing mouse hooks for Text User Interface.

---
+++ BatchToForm/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using FormsWrapper;
+using FormsWrapper.GUI;
+using FormsWrapper.TUI;
+
+namespace BatchInterpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InterceptMouse.Hook();
+            Application.Run();            
+            InterceptMouse.UnHook();
+        }
+    }
+}
